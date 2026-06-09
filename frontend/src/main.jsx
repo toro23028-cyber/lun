030fbaef36b5f27a64a78e2928995e8b9dd0eea5
toro23028-cyber <toro23028@gmail.com>
2026-06-09 02:58:21 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import AgeGate from "./AgeGate.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AgeGate>
+      <App />
+    </AgeGate>
   </StrictMode>
 );
